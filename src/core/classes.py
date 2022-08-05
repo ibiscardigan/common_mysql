a98e8a384a3d3f55a84a5429c8d664ae046b2c80
Hamish Songsmith <hamish_songsmith@icloud.com>
2022-08-05 19:23:49 +1000

    implemented core

--- src/core/classes.py
+++ src/core/classes.py
@@ -1,5 +1,4 @@
 # Standard Library Imports
-from dataclasses import dataclass
 
 # Third Party Library Imports
 
@@ -7,22 +6,84 @@ from dataclasses import dataclass
 
 # Configure Logging
 
+types = [
+    "VARCHAR",
+    "INT",
+    "FLOAT",
+    "BOOL",
+    "DATE",
+    "DATETIME",
+    "TIMESTAMP"
+]
 
-@dataclass
+
 class field():
-    name: str
-    type: str
-    length: int = None
-    default: str = None
-    null: bool = True
-    primary: bool = False
-    increment: bool = False
+    def __init__(self,
+                 name: str,
+                 field_type: str,
+                 length: int = None,
+                 default: str = None,
+                 null: bool = True,
+                 primary: bool = False,
+                 increment: bool = False) -> None:
+
+        self.name = name
+        if field_type.upper() in types:
+            self.type = field_type.upper()
+        else:
+            raise ValueError(f"FIELD TYPE UNRECOGNISED; {type(field_type)}")
+
+        if length is None or isinstance(length, int):
+            self.length = length
+        else:
+            raise TypeError(f"FIELD LENGTH NOT INT; {type(length)}")
+
+        self.default = default
+
+        if isinstance(primary, bool) is False:
+            raise TypeError(f"FIELD PRIMARY NOT BOOL; {type(primary)}")
+        else:
+            self.primary = bool(primary)
+
+        if isinstance(null, bool) is False:
+            raise TypeError(f"FIELD NULL NOT BOOL; {type(null)}")
+        else:
+            self.null = bool(null)
+        if self.primary is True and self.null is True:
+            raise ValueError("FIELD PRIMARY AND NULL CANNOT BOTH BE TRUE")
+
+        if isinstance(increment, bool) is False:
+            raise TypeError(f"FIELD INCREMENT NOT BOOL; {type(increment)}")
+        else:
+            self.increment = bool(increment)
+        if self.type != "INT" and self.increment is True:
+            raise ValueError("FIELD INCREMENT CANNOT BE TRUE IF TYPE != INT")
+
+        pass
+
+    def dict(self):
+        '''Returns the attributes as a dict'''
+        response = {}
+        for attr, val in self.__dict__.items():
+            response[attr] = val
+
+        return response
 
 
-@dataclass
 class table():
-    name: str
-    fields: list = None
+    def __init__(self, name: str, fields: list[field] = []) -> None:
+        self.name = name
+        self.fields = []
+
+        if isinstance(fields, list) is False:
+            raise TypeError(f"FIELD SUBMISSION IS NOT LIST; {type(fields)}")
+
+        for new_field in fields:
+            if isinstance(new_field, field) is True:
+                self.fields.append(new_field)
+            else:
+                raise TypeError(f"FIELDS ARE NOT ALL TYPE FIELD; {type(new_field)}")
+        pass
 
     def lookup(self, field_name: str) -> field:
         '''Looks for the field within the db, if found, returns the field object'''
@@ -30,14 +91,39 @@ class table():
         for field in self.fields:
             if field.name == field_name:
                 return field
-
         return
 
+    def dict(self):
+        '''Returns the attributes as a dict'''
+        response = {}
+        for attr, val in self.__dict__.items():
+            response[attr] = val
 
-@dataclass
+        return response
+
+    def add_field(self, new_field: field) -> None:
+        if isinstance(new_field, field) is True:
+            self.fields.append(new_field)
+        else:
+            raise TypeError(f"NEW FIELD IS NOT A FIELD OBJECT; {type(new_field)}")
+
+        return self.fields
+
+
 class database():
-    name: str
-    tables: list = None
+    def __init__(self, name: str, tables: list[table] = []) -> None:
+        self.name = name
+        self.tables = []
+
+        if isinstance(tables, list) is False:
+            raise TypeError(f"TABLES SUBMISSION IS NOT LIST; {type(tables)}")
+
+        for new_table in tables:
+            if isinstance(new_table, table) is True:
+                self.tables.append(new_table)
+            else:
+                raise TypeError(f"TABLES ARE NOT ALL TYPE TABLE; {type(new_table)}")
+        pass
 
     def lookup(self, table_name: str) -> table:
         '''Looks for the table within the db, if found, returns the table object'''
@@ -47,3 +133,19 @@ class database():
                 return table
 
         return
+
+    def dict(self):
+        '''Returns the attributes as a dict'''
+        response = {}
+        for attr, val in self.__dict__.items():
+            response[attr] = val
+
+        return response
+
+    def add_table(self, new_table: table) -> None:
+        if isinstance(new_table, table) is True:
+            self.tables.append(new_table)
+        else:
+            raise TypeError(f"NEW TABLE IS NOT A TABLE OBJECT; {type(new_table)}")
+
+        return self.tables
--- tests/tests_core/test_classes.py
+++ tests/tests_core/test_classes.py
@@ -3,7 +3,7 @@ import src.core.classes as classes
 
 expected_field = classes.field(
     name="test",
-    type="VARCHAR",
+    field_type="VARCHAR",
     length=128,
     default=None,
     null=True,
@@ -11,15 +11,125 @@ expected_field = classes.field(
     increment=False
 )
 
+fdict = {'name': 'test', 'type': 'VARCHAR', 'length': 128, 'default': None, 'primary': False, 'null': True, 'increment': False}
+
+expected_table = classes.table(
+    name="test",
+    fields=[expected_field]
+)
+
+expected_database = classes.database(
+    name="test",
+    tables=[expected_table]
+)
+
 
 class Test_field:
     def test_field(self):
-        assert classes.field("test", "VARCHAR", 128) == expected_field
+        test_field = classes.field(name="test", field_type="VARCHAR", length=128)
+        assert test_field.name == expected_field.name
+        assert test_field.type == expected_field.type
+        assert test_field.length == expected_field.length
+        assert test_field.default == expected_field.default
+        assert test_field.null == expected_field.null
+        assert test_field.primary == expected_field.primary
+        assert test_field.increment == expected_field.increment
 
-    @pytest.mark.parametrize("invalid_field", [
-        classes.field(name="test", type="VARCHAR", increment=True), # Should not be able to increment VARCHAR
-        classes.field(name="test", type="INT", null=True, primary=True, increment=True) # Should not be able to null a primary
-        ])
-    def test_invalid_fielde(self, invalid_field):
-        with pytest.raises(Exception):
-            invalid_field
+    def test_dict_output(self):
+        assert expected_field.dict() == fdict
+
+    def test_invalid_field_increment_nonint(self):
+        with pytest.raises(ValueError):
+            classes.field(name="test", field_type="VARCHAR", increment=True)  # Should not be able to increment VARCHAR
+
+    def test_invalid_field_null_primary(self):
+        with pytest.raises(ValueError):
+            classes.field(name="test", field_type="INT", null=True, primary=True, increment=True)  # Cant null a primary
+
+    def test_invalid_field_type(self):
+        with pytest.raises(ValueError):
+            classes.field(name="test", field_type="TEST")
+
+    def test_invalid_field_length(self):
+        with pytest.raises(TypeError):
+            classes.field(name="test", field_type="INT", length="test")
+
+    def test_invalid_field_null(self):
+        with pytest.raises(TypeError):
+            classes.field(name="test", field_type="INT", null="test")
+
+    def test_invalid_field_increment(self):
+        with pytest.raises(TypeError):
+            classes.field(name="test", field_type="INT", increment="test")
+
+    def test_invalid_field_primary(self):
+        with pytest.raises(TypeError):
+            classes.field(name="test", field_type="INT", primary="test")
+
+
+class Test_table:
+    def test_table(self):
+        test_table = classes.table("test", [expected_field])
+        assert test_table.name == expected_table.name
+        assert test_table.fields[0].length == expected_table.fields[0].length
+
+    def test_add_field(self):
+        expected_table.add_field(expected_field)
+        assert expected_table.fields[0].length == expected_field.length
+
+    def test_invalid_add_field(self):
+        with pytest.raises(TypeError):
+            expected_table.add_field("test")
+
+    def test_invalid_fields(self):
+        with pytest.raises(TypeError):
+            classes.table("test", "test")
+
+    def test_invalid_field(self):
+        with pytest.raises(TypeError):
+            classes.table("test", ["test"])
+
+    def test_dict_output(self):
+        expected_table.fields = []
+        assert expected_table.dict() == {'name': 'test', 'fields': []}
+
+    def test_field_lookup_null(self):
+        assert expected_table.lookup(expected_field.name) is None
+
+    def test_field_lookup(self):
+        expected_table.fields = [expected_field]
+        assert expected_table.lookup(expected_field.name).length == expected_field.length
+
+
+class Test_database:
+    def test_database(self):
+        test_database = classes.database("test", [expected_table])
+        assert test_database.name == expected_database.name
+        assert test_database.tables[0].fields[0].length == expected_field.length
+
+    def test_add_table(self):
+        expected_database.add_table(expected_table)
+        assert expected_database.tables[0].fields[0].length == expected_field.length
+
+    def test_invalid_add_table(self):
+        with pytest.raises(TypeError):
+            expected_database.add_table("test")
+
+    def test_invalid_table(self):
+        with pytest.raises(TypeError):
+            classes.database("test", "test")
+
+    def test_invalid_tables(self):
+        with pytest.raises(TypeError):
+            classes.database("test", ["test"])
+
+    def test_dict_output(self):
+        expected_database.tables = []
+        assert expected_database.dict() == {'name': 'test', 'tables': []}
+
+    def test_table_lookup_null(self):
+        assert expected_database.lookup(expected_table.name) is None
+
+    def test_field_lookup(self):
+        expected_database.tables = [expected_table]
+        assert expected_database.lookup(expected_table.name).fields[0].length == expected_field.length
